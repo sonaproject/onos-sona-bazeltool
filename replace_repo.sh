815b60f7b13e04bdd902a44349f96d6d4406c9b5
Jian Li <pyguni@gmail.com>
2020-03-16 18:29:42 +0900

    Use the first parameter as the local repo URL to be replaced

--- replace_repo.sh
+++ replace_repo.sh
@@ -2,12 +2,10 @@
 
 STABLE_ONOS=01-stable-onos
 
-source $BASH_PROFILE
-
 if [ "$#" -lt 1 ]; then
     echo "$# is Illegal number of parameters."
     echo "Usage: $0 [options]"
     exit 0
 fi
 
-sed -i 's,https://repo1.maven.org/maven2,$1,g' $STABLE_ONOS/tools/build/bazel/generate_workspace.bzl
+sed -i 's,https://repo1.maven.org/maven2,'$1',g' $STABLE_ONOS/tools/build/bazel/generate_workspace.bzl
